b8276ad20ea2f422620d349c8d41fc679971de94
benzrf <benzrf@benzrf.com>
2016-08-19 13:55:03 -0400

    Added std.sfp; accounted for variable types in the status parser

--- src/Main.hs
+++ src/Main.hs
@@ -4,23 +4,40 @@ module Main where
 import Reflex.Dom
 import Data.FileEmbed
 import Utils.Vars
+import Utils.ABT
+import DependentImplicit.Unification.Elaborator
+import DependentImplicit.Unification.Elaboration
+import DependentImplicit.Core.Term
 import DependentImplicit.Core.Parser
+import qualified Data.ByteString.Char8 as BS
 import qualified Data.Text as T
 import Control.Monad.Trans
+import Data.List
 import GHCJS.DOM.Document
 import ProofCas.Proofs
 import ProofCas.Interface
 
+Right std = parseProgram (BS.unpack $(embedFile "std.sfp"))
+Right (_, ElabState sig defs _ _ _) = runElaborator0 (elabProgram std)
+
 parseAssm code = (\t -> (FreeVar (T.unpack n), t)) <$> parseTerm (T.unpack (T.tail code'))
   where (n, code') = T.breakOn ":" code
+
+freeToDefinedModCtx :: Context -> ABT TermF -> ABT TermF
+freeToDefinedModCtx c = freeToDefined d
+  where d s
+          | FreeVar s `elem` map fst c = Var (Free (FreeVar s))
+          | otherwise = In (Defined s)
 
 parseStatus code = do
   thm:prf:ctx <- let s = T.splitOn "," code
                  in if length s >= 2 then Right s else Left "not enough parts"
-  thm' <- parseTerm (T.unpack thm)
-  prf' <- parseTerm (T.unpack prf)
   ctx' <- mapM parseAssm ctx
-  return (Status [] [] ctx' thm' prf')
+  let ctx'' = zipWith ftdmc (inits ctx') ctx'
+      ftdmc c (v, t) = (v, freeToDefinedModCtx c t)
+  thm' <- freeToDefinedModCtx ctx'' <$> parseTerm (T.unpack thm)
+  prf' <- freeToDefinedModCtx ctx'' <$> parseTerm (T.unpack prf)
+  return (Status sig defs ctx'' thm' prf')
 
 fromCode bodyEl c = case parseStatus c of
   Left err -> text (T.pack err)
